ea0796fb6a58b6f56782151b8625b82899b7139b
Clozer <alvarodgonzalo@gmail.com>
2022-09-15 20:30:23 +0200

    ⬆️ Update deps

--- src/components/users-list-view-selector.jsx
+++ src/components/users-list-view-selector.jsx
@@ -6,7 +6,7 @@ const UsersListViewSelector = ({ showRowsFormat, setShowRowsFormat }) => (
 		<button
 			onClick={() => setShowRowsFormat(false)}
 			disabled={!showRowsFormat}
-			className='text-lightgray disabled:text-black focus:shadow outline-none'
+			className='text-lightgray disabled:text-black'
 		>
 			<GridIcon className='h-8 w-8' />
 		</button>
@@ -14,7 +14,7 @@ const UsersListViewSelector = ({ showRowsFormat, setShowRowsFormat }) => (
 		<button
 			onClick={() => setShowRowsFormat(true)}
 			disabled={showRowsFormat}
-			className='text-lightgray disabled:text-black focus:shadow outline-none'
+			className='text-lightgray disabled:text-black'
 		>
 			<ListIcon className='h-8 w-8' />
 		</button>
